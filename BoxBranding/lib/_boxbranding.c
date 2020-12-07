7ecb0ac467e493d4c18d714042fa41f48a1aac7b
builder08 <75539906+builder08@users.noreply.github.com>
2020-12-07 11:17:34 +0200

    Update _boxbranding.c

--- BoxBranding/lib/_boxbranding.c
+++ BoxBranding/lib/_boxbranding.c
@@ -16,8 +16,8 @@ static PyObject* name(PyObject* self, PyObject* args) \
 } \
 
 /* Available functions */
-MAKE_STRING_FUNCTION(getVisionVersion)
-MAKE_STRING_FUNCTION(getVisionRevision)
+MAKE_STRING_FUNCTION(getOpenFIXVersion)
+MAKE_STRING_FUNCTION(getOpenFIXRevision)
 MAKE_STRING_FUNCTION(getDeveloperName)
 MAKE_STRING_FUNCTION(getBoxBrand)
 MAKE_STRING_FUNCTION(getOEVersion)
@@ -70,8 +70,8 @@ MAKE_STRING_FUNCTION(getFHDSkin)
 
 /* Module specification */
 static PyMethodDef boxbrandingMethods[] = {
-	{ "getVisionVersion", getVisionVersion, METH_NOARGS, NULL },
-	{ "getVisionRevision", getVisionRevision, METH_NOARGS, NULL },
+	{ "getOpenFIXVersion", getOpenFIXVersion, METH_NOARGS, NULL },
+	{ "getOpenFIXRevision", getOpenFIXRevision, METH_NOARGS, NULL },
 	{ "getDeveloperName", getDeveloperName, METH_NOARGS, NULL },
 	{ "getBoxBrand", getBoxBrand, METH_NOARGS, NULL },
 	{ "getOEVersion", getOEVersion, METH_NOARGS, NULL },
