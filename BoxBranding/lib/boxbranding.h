a1db029a25d1ea9b8de3016fa33eb75f510cb0c4
builder08 <75539906+builder08@users.noreply.github.com>
2020-12-07 11:22:33 +0200

    Update boxbranding.h

--- BoxBranding/lib/boxbranding.h
+++ BoxBranding/lib/boxbranding.h
@@ -10,8 +10,8 @@ int fileExist(const char* filename);
 
 char *ReadProcEntry(char *filename);
 
-char *_getVisionVersion();
-char *_getVisionRevision();
+char *_getOpenFIXVersion();
+char *_getOpenFIXRevision();
 char *_getDeveloperName();
 char *_getBoxBrand();
 char *_getOEVersion();
